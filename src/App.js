c18bc8082393198ce234a00b752cee3ece463c79
koidzt <koi27792@gmail.com>
2021-02-11 22:32:26 +0700

    Finish part clubs-type-array do not have show function and button

--- src/App.js
+++ src/App.js
@@ -4,151 +4,136 @@ import './App.css';
 function App() {
   const [clubs, setClubs] = useState([]);
   const [rounds, setRounds] = useState([]);
-  const [isShow, setIsShow] = useState(false);
 
   useEffect(() => {
-    fetch('/en.1.clubs.json', {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    })
-      .then((res) => {
-        // console.log(res);
-        return res.json();
-      })
-      .then((jsonResponse) => {
-        // console.log(jsonResponse);
-        return jsonResponse.clubs;
-      })
-      .then((dataClubs) => {
-        // console.log('Clubs =>', dataClubs);
-        let newClubs = [];
-        for (let i = 0; i < dataClubs.length; i++) {
+    const fetchData = async () => {
+      const resClub = await fetch('/en.1.clubs.json', {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+      const fetchClub = await resClub.json();
+      const fetchClubFinish = fetchClub.clubs;
+
+      const resRound = await fetch('/en.1.json', {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+      const fetchRound = await resRound.json();
+      const fetchRoundFinish = fetchRound.rounds;
+      // console.log(fetchClubFinish);
+      // console.log(fetchRoundFinish);
+      const addDataToClub = () => {
+        let newFetchClub = [];
+        for (let i = 0; i < fetchClubFinish.length; i++) {
           const newClub = {
-            ...dataClubs[i],
+            ...fetchClubFinish[i],
             played: 0,
             won: 0,
             drawn: 0,
             lost: 0,
             point: 0,
           };
-          newClubs.push(newClub);
+          newFetchClub.push(newClub);
         }
-        setClubs(newClubs);
-        return newClubs;
-      })
-      .then((newClubs) => {
-        console.log('Clunb =>', newClubs);
-      });
+        return newFetchClub;
+      };
+      const newFetchClub = addDataToClub();
+      // console.log(newFetchClub);
 
-    fetch('/en.1.json', {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    })
-      .then((res) => {
-        // console.log(res);
-        return res.json();
-      })
-      .then((jsonResponse) => {
-        // console.log(jsonResponse);
-        console.log('Rounds =>', jsonResponse.rounds);
-        setRounds(jsonResponse.rounds);
-      });
-  }, []);
+      const newClubs = [...newFetchClub];
+      for (let i = 0; i < fetchRoundFinish.length; i++) {
+        for (let j = 0; j < fetchRoundFinish[i].matches.length; j++) {
+          if (fetchRoundFinish[i].matches[j].score.ft[0] > fetchRoundFinish[i].matches[j].score.ft[1]) {
+            const indClubWon = newClubs.findIndex((club) => club.name === fetchRoundFinish[i].matches[j].team1);
+            newClubs[indClubWon] = {
+              code: newClubs[indClubWon].code,
+              country: newClubs[indClubWon].country,
+              drawn: newClubs[indClubWon].drawn,
+              lost: newClubs[indClubWon].lost,
+              name: newClubs[indClubWon].name,
+              played: newClubs[indClubWon].played + 1,
+              point: newClubs[indClubWon].point + 3,
+              won: newClubs[indClubWon].won + 1,
+            };
 
-  const show = () => {
-    setIsShow(true);
-    const newClubs = [...clubs];
-    for (let i = 0; i < rounds.length; i++) {
-      for (let j = 0; j < rounds[i].matches.length; j++) {
-        if (rounds[i].matches[j].score.ft[0] > rounds[i].matches[j].score.ft[1]) {
-          const indClubWon = newClubs.findIndex((club) => club.name === rounds[i].matches[j].team1);
-          newClubs[indClubWon] = {
-            code: newClubs[indClubWon].code,
-            country: newClubs[indClubWon].country,
-            drawn: newClubs[indClubWon].drawn,
-            lost: newClubs[indClubWon].lost,
-            name: newClubs[indClubWon].name,
-            played: newClubs[indClubWon].played + 1,
-            point: newClubs[indClubWon].point + 3,
-            won: newClubs[indClubWon].won + 1,
-          };
+            const indClubLost = newClubs.findIndex((club) => club.name === fetchRoundFinish[i].matches[j].team2);
+            newClubs[indClubLost] = {
+              code: newClubs[indClubLost].code,
+              country: newClubs[indClubLost].country,
+              drawn: newClubs[indClubLost].drawn,
+              lost: newClubs[indClubLost].lost + 1,
+              name: newClubs[indClubLost].name,
+              played: newClubs[indClubLost].played + 1,
+              point: newClubs[indClubLost].point,
+              won: newClubs[indClubLost].won,
+            };
+          } else if (fetchRoundFinish[i].matches[j].score.ft[0] < fetchRoundFinish[i].matches[j].score.ft[1]) {
+            const indClubWon = newClubs.findIndex((club) => club.name === fetchRoundFinish[i].matches[j].team2);
+            newClubs[indClubWon] = {
+              code: newClubs[indClubWon].code,
+              country: newClubs[indClubWon].country,
+              drawn: newClubs[indClubWon].drawn,
+              lost: newClubs[indClubWon].lost,
+              name: newClubs[indClubWon].name,
+              played: newClubs[indClubWon].played + 1,
+              point: newClubs[indClubWon].point + 3,
+              won: newClubs[indClubWon].won + 1,
+            };
 
-          const indClubLost = newClubs.findIndex((club) => club.name === rounds[i].matches[j].team2);
-          newClubs[indClubLost] = {
-            code: newClubs[indClubLost].code,
-            country: newClubs[indClubLost].country,
-            drawn: newClubs[indClubLost].drawn,
-            lost: newClubs[indClubLost].lost + 1,
-            name: newClubs[indClubLost].name,
-            played: newClubs[indClubLost].played + 1,
-            point: newClubs[indClubLost].point,
-            won: newClubs[indClubLost].won,
-          };
-        } else if (rounds[i].matches[j].score.ft[0] < rounds[i].matches[j].score.ft[1]) {
-          const indClubWon = newClubs.findIndex((club) => club.name === rounds[i].matches[j].team2);
-          newClubs[indClubWon] = {
-            code: newClubs[indClubWon].code,
-            country: newClubs[indClubWon].country,
-            drawn: newClubs[indClubWon].drawn,
-            lost: newClubs[indClubWon].lost,
-            name: newClubs[indClubWon].name,
-            played: newClubs[indClubWon].played + 1,
-            point: newClubs[indClubWon].point + 3,
-            won: newClubs[indClubWon].won + 1,
-          };
+            const indClubLost = newClubs.findIndex((club) => club.name === fetchRoundFinish[i].matches[j].team1);
+            newClubs[indClubLost] = {
+              code: newClubs[indClubLost].code,
+              country: newClubs[indClubLost].country,
+              drawn: newClubs[indClubLost].drawn,
+              lost: newClubs[indClubLost].lost + 1,
+              name: newClubs[indClubLost].name,
+              played: newClubs[indClubLost].played + 1,
+              point: newClubs[indClubLost].point,
+              won: newClubs[indClubLost].won,
+            };
+          } else {
+            const indClubTeam1 = newClubs.findIndex((club) => club.name === fetchRoundFinish[i].matches[j].team1);
+            newClubs[indClubTeam1] = {
+              code: newClubs[indClubTeam1].code,
+              country: newClubs[indClubTeam1].country,
+              drawn: newClubs[indClubTeam1].drawn + 1,
+              lost: newClubs[indClubTeam1].lost,
+              name: newClubs[indClubTeam1].name,
+              played: newClubs[indClubTeam1].played + 1,
+              point: newClubs[indClubTeam1].point + 1,
+              won: newClubs[indClubTeam1].won,
+            };
 
-          const indClubLost = newClubs.findIndex((club) => club.name === rounds[i].matches[j].team1);
-          newClubs[indClubLost] = {
-            code: newClubs[indClubLost].code,
-            country: newClubs[indClubLost].country,
-            drawn: newClubs[indClubLost].drawn,
-            lost: newClubs[indClubLost].lost + 1,
-            name: newClubs[indClubLost].name,
-            played: newClubs[indClubLost].played + 1,
-            point: newClubs[indClubLost].point,
-            won: newClubs[indClubLost].won,
-          };
-        } else {
-          const indClubTeam1 = newClubs.findIndex((club) => club.name === rounds[i].matches[j].team1);
-          newClubs[indClubTeam1] = {
-            code: newClubs[indClubTeam1].code,
-            country: newClubs[indClubTeam1].country,
-            drawn: newClubs[indClubTeam1].drawn + 1,
-            lost: newClubs[indClubTeam1].lost,
-            name: newClubs[indClubTeam1].name,
-            played: newClubs[indClubTeam1].played + 1,
-            point: newClubs[indClubTeam1].point + 1,
-            won: newClubs[indClubTeam1].won,
-          };
-
-          const indClubTeam2 = newClubs.findIndex((club) => club.name === rounds[i].matches[j].team2);
-          newClubs[indClubTeam2] = {
-            code: newClubs[indClubTeam2].code,
-            country: newClubs[indClubTeam2].country,
-            drawn: newClubs[indClubTeam2].drawn + 1,
-            lost: newClubs[indClubTeam2].lost,
-            name: newClubs[indClubTeam2].name,
-            played: newClubs[indClubTeam2].played + 1,
-            point: newClubs[indClubTeam2].point + 1,
-            won: newClubs[indClubTeam2].won,
-          };
+            const indClubTeam2 = newClubs.findIndex((club) => club.name === fetchRoundFinish[i].matches[j].team2);
+            newClubs[indClubTeam2] = {
+              code: newClubs[indClubTeam2].code,
+              country: newClubs[indClubTeam2].country,
+              drawn: newClubs[indClubTeam2].drawn + 1,
+              lost: newClubs[indClubTeam2].lost,
+              name: newClubs[indClubTeam2].name,
+              played: newClubs[indClubTeam2].played + 1,
+              point: newClubs[indClubTeam2].point + 1,
+              won: newClubs[indClubTeam2].won,
+            };
+          }
+          setClubs(newClubs);
         }
-        setClubs(newClubs);
       }
-    }
-  };
+      setRounds(fetchRoundFinish);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
       <div className="container">
         <h2>Premier League 2017/18</h2>
-        <button className="" onClick={show} disabled={isShow}>
-          Show
-        </button>
+
         <table className="table container">
           <thead>
             <tr>
@@ -162,7 +147,7 @@ function App() {
           </thead>
           <tbody>
             {clubs.map((club) => (
-              <tr>
+              <tr key={club.name}>
                 <td>{club.name}</td>
                 <td>{club.played}</td>
                 <td>{club.won}</td>
