1b715a76e7c39fe934ea6b13ef11204394b5d137
koidzt <koi27792@gmail.com>
2021-02-15 11:31:43 +0700

    Code Refactoring

--- src/App.js
+++ src/App.js
@@ -2,86 +2,112 @@ import { useEffect, useState } from 'react';
 import './App.css';
 const _ = require('lodash');
 
+//Fetch Clubs
+const fetchClubsData = async () => {
+  let resClubs = await fetch('/en.1.clubs.json', {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  });
+  resClubs = await resClubs.json();
+  resClubs = resClubs.clubs;
+
+  return resClubs;
+};
+
+//Fetch Rounds
+const fetchRoundsData = async () => {
+  let resRounds = await fetch('/en.1.json', {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  });
+  resRounds = await resRounds.json();
+  resRounds = resRounds.rounds;
+
+  return resRounds;
+};
+
+//Created Match Result for each Clubs
+const matchResultsForEachClub = (resClubs) => {
+  let matchResults = {};
+  _.forEach(resClubs, (club) => {
+    matchResults[club.name] = {
+      name: club.name,
+      played: 0,
+      won: 0,
+      drawn: 0,
+      lost: 0,
+      point: 0,
+    };
+  });
+  // console.log(matchResults);
+
+  return matchResults;
+};
+
+//Updated Match Results
+const updatedMatchResults = (newMatchResults, resRounds) => {
+  let matchResults = { ...newMatchResults };
+  _.forEach(resRounds, (round) => {
+    _.forEach(round.matches, (match) => {
+      if (match.score.ft[0] === match.score.ft[1]) {
+        const drawnClubs = [match.team1, match.team2];
+        _.forEach(drawnClubs, (name) => {
+          matchResults[name] = {
+            ...matchResults[name],
+            played: matchResults[name].played + 1,
+            drawn: matchResults[name].drawn + 1,
+            point: matchResults[name].point + 1,
+          };
+        });
+      } else {
+        const clubWin = match.score.ft[0] > match.score.ft[1] ? match.team1 : match.team2;
+        // Win
+        matchResults[clubWin] = {
+          ...matchResults[clubWin],
+          played: matchResults[clubWin].played + 1,
+          won: matchResults[clubWin].won + 1,
+          point: matchResults[clubWin].point + 3,
+        };
+
+        const clubLost = match.score.ft[0] < match.score.ft[1] ? match.team1 : match.team2;
+        //Lost
+        matchResults[clubLost] = {
+          ...matchResults[clubLost],
+          played: matchResults[clubLost].played + 1,
+          lost: matchResults[clubLost].lost + 1,
+        };
+      }
+    });
+  });
+
+  return matchResults;
+};
+
+//Sort Match Results By Points and Change Object to Array
+const sortRankForEachClub = (matchResults) => {
+  const sortPointsByDesc = _.sortBy(matchResults, 'point').reverse();
+  return sortPointsByDesc;
+};
+
 function App() {
-  // const [clubs, setClubs] = useState([]);
-  // const [rounds, setRounds] = useState([]);
+  const [clubs, setClubs] = useState([]);
+  const [rounds, setRounds] = useState([]);
   const [sumClubs, setSumClubs] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      let resClubs = await fetch('/en.1.clubs.json', {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      });
-      resClubs = await resClubs.json();
-      resClubs = resClubs.clubs;
+  useEffect(async () => {
+    const fetchClubs = await fetchClubsData();
+    const fetchRounds = await fetchRoundsData();
+    const newMatchResults = matchResultsForEachClub(fetchClubs);
+    const matchResults = updatedMatchResults(newMatchResults, fetchRounds);
+    const rankClubs = sortRankForEachClub(matchResults);
 
-      let resRounds = await fetch('/en.1.json', {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      });
-      resRounds = await resRounds.json();
-      resRounds = resRounds.rounds;
-
-      let matchResults = {};
-      _.forEach(resClubs, (club) => {
-        matchResults[club.name] = {
-          name: club.name,
-          played: 0,
-          won: 0,
-          drawn: 0,
-          lost: 0,
-          point: 0,
-        };
-      });
-      // console.log(matchResults);
-
-      _.forEach(resRounds, (round) => {
-        _.forEach(round.matches, (match) => {
-          if (match.score.ft[0] === match.score.ft[1]) {
-            const drawnClubs = [match.team1, match.team2];
-            _.forEach(drawnClubs, (name) => {
-              matchResults[name] = {
-                ...matchResults[name],
-                played: matchResults[name].played + 1,
-                drawn: matchResults[name].drawn + 1,
-                point: matchResults[name].point + 1,
-              };
-            });
-          } else {
-            const clubWin = match.score.ft[0] > match.score.ft[1] ? match.team1 : match.team2;
-            // Win
-            matchResults[clubWin] = {
-              ...matchResults[clubWin],
-              played: matchResults[clubWin].played + 1,
-              won: matchResults[clubWin].won + 1,
-              point: matchResults[clubWin].point + 3,
-            };
-
-            const clubLost = match.score.ft[0] < match.score.ft[1] ? match.team1 : match.team2;
-            //Lost
-            matchResults[clubLost] = {
-              ...matchResults[clubLost],
-              played: matchResults[clubLost].played + 1,
-              lost: matchResults[clubLost].lost + 1,
-            };
-          }
-        });
-      });
-
-      const newMatchResults = _.sortBy(matchResults, 'point').reverse();
-      // console.log(newMatchResults);
-
-      setSumClubs(newMatchResults);
-      // setClubs(resClubs);
-      // setRounds(resRounds);
-    };
-
-    fetchData();
+    setClubs(fetchClubs);
+    setRounds(fetchRounds);
+    setSumClubs(rankClubs);
   }, []);
 
   return (
